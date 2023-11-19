45b9fdae3497923be4714b2d51dc96b966cd8782
Daiyaan Muhammad <daiyaan.villain@gmail.com>
2023-11-19 12:57:22 +0600

    Project Started on 19 November, 2023

---
+++ src/main/java/com/gamma/Gamma/GammaApplication.java
@@ -0,0 +1,13 @@
+package com.gamma.Gamma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GammaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GammaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamma/Gamma/GammaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamma.Gamma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GammaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
